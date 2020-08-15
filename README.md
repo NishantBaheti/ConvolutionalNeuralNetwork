187aaecfbfc1c2f02b2352c778b13e1e8e853b09
Nishant Baheti <nishantbaheti.it19@gmail.com>
2020-08-16 03:13:13 +0530

    updates

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Convolutional_Neural_Network
+
+## Application level Configurations
+
+### Creating a self signed certificate
+
+- openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out example.crt -keyout example.key
+
+### Starting development server with https
+
+- HTTPS=true SSL_CRT_FILE=./cert/cvapp.crt SSL_KEY_FILE=./cert/cvapp.key npm start
+
+### Setting up Tomcat with https
+
+```If you want to generate an SSL cert for development purposes for use with tomcat, you can do it using this one liner (requires JDK on your machine, so this doesnt use openssl).
+
+```
+
+- keytool -genkey -keyalg RSA -noprompt -alias tomcat -dname "CN=localhost, OU=NA, O=NA, L=NA, S=NA, C=NA" -keystore keystore.jks -validity 9999 -storepass changeme -keypass changeme
+
+```
+This generates a keystore.jks file with a password of changeme using a keyAlias of tomcat that's valid for 9999 days for localhost
+
+In your tomcat/conf/server.xml, you'd need to specify it like so in your <Connector>
+
+    keyAlias="tomcat"
+    keystoreFile="/path/to/my/keystore.jks"
+    keystorePass="changeme"
+
+Since Tomcat 8
+
+According to the documentation:
+
+NIO and NIO2 SSL configuration attributes have been deprecated in favor of the default SSLHostConfig
+
+This means the values above should now be put as attributes of connector/SSLHostConfig/Certificate with these names:
+
+    certificateKeyAlias="tomcat"
+    certificateKeystoreFile="/path/to/my/keystore.jks"
+    certificateKeystorePassword="changeme"
+```
+
+### open port
+
+- sudo ufw allow 8443
--- cvapp/src/components/CamCaptureutil.js
+++ cvapp/src/components/CamCaptureutil.js
@@ -1,63 +1,82 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import Webcam from "react-webcam";
 import axios from "axios";
 
 export default class CamCaptureutil extends Component {
   state = {
-    imageCaptured : "",
-    prediction : ""
-  }
-  setRef = webcam => {
-    this.webcam = webcam
+    imageCaptured: "",
+    prediction: "",
+    facingMode: "environment",
+  };
+  setRef = (webcam) => {
+    this.webcam = webcam;
+  };
+  cameraHandler = () => {
+    if (this.state.facingMode === "environment") {
+      this.setState({
+        facingMode: "user",
+      });
+    } else {
+      this.setState({
+        facingMode: "environment",
+      });
+    }
   };
   captureImageHandler = () => {
     const imageSrc = this.webcam.getScreenshot();
     this.setState({
-      imageCaptured : imageSrc,
-    })
-    console.log(imageSrc)
-  }
+      imageCaptured: imageSrc,
+    });
+    console.log(imageSrc);
+  };
   uploadImageHandler = () => {
     if (this.state.imageCaptured === "") {
       this.setState({
-        prediction : "No Image Camptured"
+        prediction: "No Image Camptured",
       });
-    }
-    else{
+    } else {
       var imageObj = {
-        "imageString" : this.state.imageCaptured
-      }
-      axios.post(this.props.customConfig.predictionUrl+"/getPredictionFromImageString",imageObj).then((res) => {
-        console.log(res.data);
-        this.setState({
-          prediction : res.data.result
+        imageString: this.state.imageCaptured,
+      };
+      axios
+        .post(
+          this.props.customConfig.predictionUrl +
+            "/getPredictionFromImageString",
+          imageObj
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.setState({
+            prediction: res.data.result,
+          });
         });
-      });
     }
-  }
-  render(){
+  };
+  render() {
     const videoConstraints = {
       width: 720,
       height: 720,
-      facingMode: "user"
+      facingMode: this.state.facingMode,
     };
-    return(
-        <div>
-          < Webcam
-              audio={false}
-              height={64*6}
-              ref = {this.setRef}
-              screenshotFormat="image/jpeg"
-              width={64*6}
-              videoConstraints={videoConstraints}
-          />
-          <img src={this.state.imageCaptured}/>
-          <button onClick = {this.captureImageHandler}> Capture </button>
-          <button onClick = {this.uploadImageHandler}> Predict </button>
-          <span>
-            <h3>{this.state.prediction}</h3>
-          </span>
-        </div>
-    )
+    return (
+      <div>
+        <Webcam
+          audio={false}
+          height={64 * 6}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={64 * 6}
+          mirrored={false}
+          videoConstraints={videoConstraints}
+        />
+        <img src={this.state.imageCaptured} />
+        <button onClick={this.cameraHandler}>Change Camera</button>
+        <button onClick={this.captureImageHandler}> Capture </button>
+        <button onClick={this.uploadImageHandler}> Predict </button>
+        <span>
+          <h3>{this.state.prediction}</h3>
+        </span>
+      </div>
+    );
   }
 }
